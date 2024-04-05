ff709793cf44442afb685b91b9f38f5a5c653682
zwill <zwill_1@hotmail.com>
2024-04-05 00:16:04 -0600

    adding modal for creation of product

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include "db/dbcon.php" ?>
 <?php include "assets/header.php" ?>
-    <section>
+    <section class="mt-5 mb-2">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Product</button>
+    </section>
+    <section class="mb-5">
         <form method="POST" action="">
             <div class="row mb-3">
                 <div class="col">
@@ -71,4 +74,47 @@
             </tbody>
         </table>
     </section>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <form action="" method="">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add A New Product</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <!-- <form> -->
+                    <div class="form-group">
+                        <label for="exampleInputFistName">Product Name</label>
+                        <input type="text" class="form-control" id="exampleInputFistName" name="pname" aria-describedby="First Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputLastName">Product Price</label>
+                        <input type="text" class="form-control" id="exampleInputLastName" name="pprice">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputAge">Product Quantity</label>
+                        <input type="text" class="form-control" id="exampleInputAge" name="pquantity">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputAge">Product Image Link</label>
+                        <input type="text" class="form-control" id="exampleInputAge" name="pimage">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputAge">Product Description</label>
+                        <input type="text" class="form-control" id="exampleInputAge" name="pdescription">
+                    </div>
+                <!-- </form> -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <input type="submit" class="btn btn-success" name="" value="Add" />
+                </div>
+                </div>
+            </form>
+        </div>
+        </div>
 <?php include "assets/footer.php" ?>
