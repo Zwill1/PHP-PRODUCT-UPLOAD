742665595f6429d035ac446355b1062556688013
zwill <zwill_1@hotmail.com>
2024-04-11 23:45:42 -0600

    setting up product profile

--- db/product.php
+++ db/product.php
@@ -29,9 +29,50 @@
             </div>
             <div class="col-8">
                 <h1 class="fw-bold"><?php echo $row['prodname']; ?></h1>
+                <p>Brand: -- Brand --</p>
+                <div class="d-flex justify-content-start" style="background-color: red;">
+                        <p>--rating-- --star rating--</p>
+                        <p>--total rating bumber-- ratings</p>
+                </div>
                 <p>$<?php echo $row['prodprice'] ?></p>
                 <p>Quantity remaining: <?php echo $row['prodquantity'] ?></p>
                 <p><?php echo $row['proddescription'] ?></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-3 mb-3">
+        <div class="row">
+            <div class="col-12">
+                <h2>Product Description</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-3 mb-3">
+        <div class="row">
+            <div class="col-12">
+                <h4>What do customers buy after viewing this item?</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                --Card--
+            </div>
+            <div class="col-2">
+                --Card--
+            </div>
+            <div class="col-2">
+                --Card--
+            </div>
+            <div class="col-2">
+                --Card--
+            </div>
+            <div class="col-2">
+                --Card--
+            </div>
+            <div class="col-2">
+                --Card--
             </div>
         </div>
     </div>
