6d5101d4575b758a448a62fce95c03e1f7bacc4e
zwill <zwill_1@hotmail.com>
2024-04-14 22:19:03 -0600

    working on buying product cards on product page

--- db/product.php
+++ db/product.php
@@ -50,13 +50,41 @@
     </div>
 
     <div class="mt-3 mb-3">
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-12">
                 <h4>What do customers buy after viewing this item?</h4>
             </div>
         </div>
-        <div class="row">
-            <div class="col-2">
+        <div class="row mt-3">
+            <?php 
+            
+            $query = "SELECT * FROM products";
+
+            $result = mysqli_query($connection, $query);
+
+            if(!$result){
+                die("Query failed" . mysqli_error($connection));
+            }
+            else {
+                for($i = 1; $i <= 6; $i++){
+                    $row = mysqli_fetch_assoc($result);
+                    ?>
+
+                    <div class='col-2'>
+                        <img src="<?php echo $row['prodimage']; ?>" class="card-img-top" alt="<?php echo $row['prodname']; ?>" style="width: 150px;">
+                        <h4 class="mb-1"><?php echo $row['prodname']?></h4>
+                        <p class="">$<?php echo $row['prodprice']; ?></p>
+                    </div>
+
+                    <?php
+                }
+            }
+            ?>
+            
+            <!-- <div class="col-2">
+                --Card--
+            </div> -->
+            <!-- <div class="col-2">
                 --Card--
             </div>
             <div class="col-2">
@@ -70,10 +98,7 @@
             </div>
             <div class="col-2">
                 --Card--
-            </div>
-            <div class="col-2">
-                --Card--
-            </div>
+            </div> -->
         </div>
     </div>
 
